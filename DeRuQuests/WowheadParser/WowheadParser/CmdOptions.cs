9bc509863699b3b527302aefcf319788806c2fcd
Kyrylo M <kirmir@outlook.com>
2015-09-04 21:44:05 +0300

    Added project for parser

---
+++ DeRuQuests/WowheadParser/WowheadParser/CmdOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace WowheadParser
+{
+    internal class CmdOptions
+    {
+        [Option('f', "from", Required = true, HelpText = "The original language to find the translation.")]
+        public string FromLanguage { get; set; }
+
+        [Option('t', "to", Required = true, HelpText = "The target language to find the translation.")]
+        public string ToLanguage { get; set; }
+    }
+}
