2b09e8fa6c4908569d181379cbcb81bf407a373b
Kyrylo M <kirmir@outlook.com>
2015-09-24 21:59:58 +0300

    Created an addon to move the standard raid frame grip.

---
+++ MoveCompacRaidFrame/MoveCompacRaidFrame.lua
@@ -0,0 +1,1 @@
+local function c(s)s:SetPoint("TOPLEFT",UIParent,"TOPLEFT",-182,-300)end hooksecurefunc("CompactRaidFrameManager_UpdateShown",c)hooksecurefunc("CompactRaidFrameManager_Expand",function e(s)s:SetPoint("TOPLEFT",UIParent,"TOPLEFT",-7,-300)end)hooksecurefunc("CompactRaidFrameManager_Collapse",c)
